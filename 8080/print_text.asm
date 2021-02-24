29f92e7c75105f8357cfe0c0809cc4ce0a76d68b
Puszka <kamilharaf7@gmail.com>
2021-02-24 16:28:18 +0100

    Refactoring

---
+++ 8080/print_text.asm
@@ -0,0 +1,5 @@
+	 ORG 800H  
+	 LXI H,TEXT  
+	 RST 3  
+	 HLT  
+TEXT 	 DB 'print me',13,10,'please',13,10,41h,'@'     
--- 8080/wypiszTekst.asm
+++
@@ -1,5 +0,0 @@
-	 ORG 800H  
-	 LXI H,TEKST  
-	 RST 3  
-	 HLT  
-TEKST 	 DB 'jakis tekst',13,10,'jakis inny',13,10,41h,'@'     
